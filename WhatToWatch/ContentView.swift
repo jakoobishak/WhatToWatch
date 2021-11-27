4958ca72d4acc307d1fc5dd0428013e2095a2a5c
Jakoob Ishak <jakoob_ishak@hotmail.com>
2021-11-27 13:41:56 +0100

    t

--- WhatToWatch/ContentView.swift
+++ WhatToWatch/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
-        Text("Jakoob")
+        Text("Kyrollos!!!")
     }
 
     private func addItem() {
